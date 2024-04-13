0d5737ab1dff67dabbc0d42e0a05e72ccc2ef12d
Ramakrishnadulam <166901738+Ramakrishnadulam@users.noreply.github.com>
2024-04-13 13:21:46 +0530

    Update variables.py

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -38,3 +38,57 @@ print('Last name: ', last_name)
 print('Country: ', country)
 print('Age: ', age)
 print('Married: ', is_married)
+
+
+first_name = 'Ramakrishna'
+last_name = 'Dulam'
+
+print (type(first_name))
+print (type(last_name))
+print (len(first_name))
+print (len(last_name))
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+print(total)
+diff = num_one - num_two
+print(diff)
+product = num_one*num_two
+print(product)
+division = num_one/num_two
+print(division)
+remainder = num_one%num_two
+print(remainder)
+exp = num_one ** num_two
+print(exp)
+floor_division = num_one//num_two
+print(floor_division)
+
+radius_circle = 30
+PI = 3.14
+area_of_circle = PI * radius_circle ** 2
+print(area_of_circle)
+circum_of_circle = 2 * PI * radius_circle
+print(circum_of_circle)
+
+radius_circle = input("Enter the radius of the Circle: ")
+print(area_of_circle)
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+help('keywords')
+
+
+
+
+
+
+
+
+
+
+
+
+
